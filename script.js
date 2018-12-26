3a7a652c2939bed0ab3c9c6a6abbea6b660b90a3
Adel <adelsd69@gmail.com>
2018-12-27 00:52:02 +0600

    7.1
    responsive full

---
+++ script.js
@@ -0,0 +1,20 @@
+function openNav()
+{
+document.querySelector('.responsive__nav').style.display = 'grid' ; 
+document.querySelector('.responsive__nav').style.opacity = '1' ;
+
+
+
+
+
+}
+function closeNav()
+{
+document.querySelector('.responsive__nav').style.display = 'none' ; 
+
+document.querySelector('.responsive__nav').style.opacity = '0' ; 
+
+
+
+
+}
