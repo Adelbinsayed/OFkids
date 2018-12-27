72a716fe8060c7268efc89fd40e0b24a65c5f0c9
Adel <adelsd69@gmail.com>
2018-12-27 14:56:59 +0600

    7.4.1

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function openNav()
 {
-document.querySelector('.responsive__nav').style.display = 'grid' ; 
-document.querySelector('.responsive__nav').style.opacity = '1' ;
-
-
+//document.querySelector('.responsive__nav').style.display = 'grid' ; 
+document.querySelector('.responsive__nav').style.opacity = '1'  ;
+document.getElementById('nav__responsive').style.width = '50rem' ;
+document.getElementById('nav__responsive').style.visibility = 'visible' ;
 
 
 
 }
 function closeNav()
 {
-document.querySelector('.responsive__nav').style.display = 'none' ; 
-
+//document.querySelector('.responsive__nav').style.display = 'none' ; 
+document.getElementById('nav__responsive').style.width = '0' ;
 document.querySelector('.responsive__nav').style.opacity = '0' ; 
-
+document.getElementById('nav__responsive').style.visibility = 'hidden' ;
 
 
 
